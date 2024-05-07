c21caa3e4c5b8b8ff3431caa5d68d124503d1b2d
Logan <148842222+Kantooo@users.noreply.github.com>
2024-05-07 07:58:55 +0300

    Delete api-rest-V.F

--- api-rest-V.F/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config(); 
-const app = require('./app');
-const port = process.env.PORT;
-
-app.listen(port, function(){
-    console.log(`L'application fonctionne sur le port ${port}`);
-});
--- api-rest-V.F/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Route pour l'authentification par email
-router.post('/login', authController.login);
-
-module.exports = router;
